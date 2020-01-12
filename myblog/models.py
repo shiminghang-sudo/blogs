830283504b2f42bf3e40db103251698f92578046
shiminghang_sudo <1518532557@qq.com>
2020-01-13 01:51:37 +0800

    更新数据

--- myblog/models.py
+++ myblog/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=60, default='title')
     content = models.TextField(null=True)
-# Create your models here.
+
